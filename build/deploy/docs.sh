34346ee4339a6ac7a513f510acf6728798bfe53b
zhuhong <qbadboy@gmail.com>
2019-04-02 14:45:33 +0800

    travis config

--- build/deploy/docs.sh
+++ build/deploy/docs.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+set -e
+
 git pull origin master
 npm run build:docs
 
-git add docs/.vuepress/dist
+cd docs/.vuepress/dist
+
+git init
+git add -A
 git commit -m "build document"
 
-git push origin gh-pages
+git config --local user.name "zhuhong"
+git config --local user.email "qbadboy@gmail.com"
+
+git push -f https://${access_token}@github.com/qbadboy/carp-ui.git master:gh-pages
