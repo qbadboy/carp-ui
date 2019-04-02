d2fe55fd5564fa43b363c8c00a93e422cf0fd88a
zhuhong <qbadboy@gmail.com>
2019-04-02 15:20:07 +0800

    docs init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  title: 'CARP-UI'
+  title: 'CARP-UI',
+  description: 'A beautiful ui Library',
+  base: '/carp-ui/',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/'}
+    ],
+    sidebar: [
+      {title: '介绍', path: '/'}
+    ]
+  }
 }
